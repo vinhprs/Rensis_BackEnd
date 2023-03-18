560d45e645013ca9dbc4469ab20f251d2e882e8e
Vinh Nguyễn <vinhnguyen@Vinhs-MacBook-Pro.local>
2023-03-18 13:54:20 +0700

    education module

--- src/modules/educations/educations.resolver.ts
+++ src/modules/educations/educations.resolver.ts
@@ -1,35 +1,26 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { EducationsService } from './educations.service';
 import { Education } from './entities/education.entity';
 import { CreateEducationInput } from './dto/create-education.input';
 import { UpdateEducationInput } from './dto/update-education.input';
+import { BadRequestException } from '@nestjs/common';
+import { Request } from 'express';
 
 @Resolver(() => Education)
 export class EducationsResolver {
-  constructor(private readonly educationsService: EducationsService) {}
-
+  constructor(
+    private readonly educationsService: EducationsService
+  ) {}
+  
   @Mutation(() => Education)
-  createEducation(@Args('createEducationInput') createEducationInput: CreateEducationInput) {
-    return this.educationsService.create(createEducationInput);
-  }
-
-  @Query(() => [Education], { name: 'educations' })
-  findAll() {
-    return this.educationsService.findAll();
-  }
-
-  @Query(() => Education, { name: 'education' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.educationsService.findOne(id);
-  }
-
-  @Mutation(() => Education)
-  updateEducation(@Args('updateEducationInput') updateEducationInput: UpdateEducationInput) {
-    return this.educationsService.update(updateEducationInput.id, updateEducationInput);
-  }
-
-  @Mutation(() => Education)
-  removeEducation(@Args('id', { type: () => Int }) id: number) {
-    return this.educationsService.remove(id);
+  async createEducation(
+    @Args('createEducationInput') createEducationInput: CreateEducationInput,
+    @Context('req') req: Request
+  ) : Promise<Education> {
+    try {
+      return await this.educationsService.createEducation(createEducationInput, req);
+    } catch(e) {
+      throw new BadRequestException(e.message);
+    }
   }
 }
